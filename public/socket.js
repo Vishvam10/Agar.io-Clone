9220d791c31f343e3e7d3c2837b65a4c5f958ff2
Vishvam <s.vishvam1025@gmail.com>
2022-09-03 22:51:21 +0530

    Added leaderboard on the client

--- public/socket.js
+++ public/socket.js
@@ -23,10 +23,23 @@ socket.on("initReturn", (data) => {
 
 socket.on("tock", (data) => {
     players = data.players;
-    player.locX = data.playerX;
-    player.locY = data.playerY;
 })
+
+socket.on('tickTock',(data)=>{
+    player.locX = data.playerX
+    player.locY = data.playerY
+})
+
 
 socket.on("orbSwitch", (data) => {
     orbs.splice(data.orbIndex, 1, data.newOrb);
+})
+
+socket.on('updateLeaderBoard',(data)=>{
+    document.querySelector('.leader-board').innerHTML = "";
+    data.forEach((curPlayer)=>{
+        document.querySelector('.leader-board').innerHTML += `
+            <li class="leaderboard-player">${curPlayer.name} - ${curPlayer.score}</li>
+        `
+    })
 })
